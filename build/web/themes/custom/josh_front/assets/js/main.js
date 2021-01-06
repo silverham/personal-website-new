9cd108156535126c8cda95f0571bd9440261cf3b
Joshua Graham <jgcomms@outlook.com.au>
2021-01-06 20:00:50 +1100

    Create 404 and 403 pages + remove metatag page title token since it doesn't work with views title token @see https://www.drupal.org/project/token/issues/2665980

--- build/web/themes/custom/josh_front/assets/js/main.js
+++ build/web/themes/custom/josh_front/assets/js/main.js
@@ -1,31 +1,39 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports["default"] = _default;
 
-exports.default = function () {
+/**
+ * @file
+ * Module.js.
+ */
+
+/**
+ * Default function.
+ */
+function _default() {
   'use strict';
   /* Code here */
-};
+}
+
+module.exports = exports.default;
 
 },{}],2:[function(require,module,exports){
 /**
  * @file
  * Main.js.
  */
-
 'use strict';
 
-var _featureA = require('./feature-a/feature-a.js');
+var _featureA = _interopRequireDefault(require("./feature-a/feature-a.js"));
 
-var _featureA2 = _interopRequireDefault(_featureA);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 // Run default function from js file.
-(0, _featureA2.default)();
+(0, _featureA["default"])();
 
 },{"./feature-a/feature-a.js":1}]},{},[2])
 
